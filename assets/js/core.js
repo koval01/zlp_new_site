957ce9b71dfb07af4af097fa9e25d7dd9b85bfd3
koval01 <yaroslav@koval.page>
2022-08-18 21:18:10 +0200

    update

--- assets/js/core.js
+++ assets/js/core.js
@@ -151,7 +151,7 @@ function monitoring_game_server_update() {
 function game_server_updater() {
     monitoring_game_server_update();
     setInterval(monitoring_game_server_update, 3000)
-}
+};
 
 function get_donate_services(callback) {
     $.ajax({
@@ -168,7 +168,7 @@ function get_donate_services(callback) {
             console.log("Error get donate services list")
         }
     })
-}
+};
 
 function create_payment(callback, customer, products, email = null, coupon = null) {
     $.ajax({
@@ -193,11 +193,11 @@ function create_payment(callback, customer, products, email = null, coupon = nul
             console.log("Error get donate services list")
         }
     })
-}
+};
 
 function append_services() {
     get_donate_services(function (services) {
-        donate_services_array = services
+        donate_services_array = services;
         for (let i = 0; i < services.length; i++) {
             let click_data = {
                 "name": services[i].name,
@@ -206,7 +206,7 @@ function append_services() {
                 "description": services[i].description,
                 "type": services[i].type,
                 "service_id": services[i].id
-            }
+            };
             $("#donate_items_list").append(`
                 <div class="col" id="donate_item_${services[i].id}">
                     <div class="card card-hover border-0 bg-transparent" 
@@ -242,73 +242,73 @@ function append_services() {
                     </div>
                 </div>
             `)
-        }
+        };
 
         setTimeout(function () {
-            $("#donate_block_load").remove()
+            $("#donate_block_load").remove();
             $("#donate_items_list").css("display", "")
         }, 100)
     })
-}
+};
 
 function redirect_(url) {
     return window.location.replace(url)
-}
+};
 
 function modal_close_() {
-    _body.removeClass("modal-open")
+    _body.removeClass("modal-open");
     document.getElementById("donate_item_modal").style.display = "none"
-}
+};
 
 function switch_modal_containers(mode = "service") {
-    const span = document.getElementsByClassName("close_b")
-    const info = $("#modal-info-container-c")
-    const service = $("#modal-donate-container-c")
-    const finish_donate = $("#modal-donate-finish-container-c")
+    const span = document.getElementsByClassName("close_b");
+    const info = $("#modal-info-container-c");
+    const service = $("#modal-donate-container-c");
+    const finish_donate = $("#modal-donate-finish-container-c");
     const _array = [
         {"name": "service", "selector": service},
         {"name": "info", "selector": info},
         {"name": "donate_finish", "selector": finish_donate}
-    ]
+    ];
     for (let i = 0; i < _array.length; i++) {
-        let _mode = "none"
+        let _mode = "none";
         if (mode === _array[i].name) {
-            _mode = "block"
+            _mode = "block";
             span[i].onclick = function () {
                 modal_close_()
             }
-        }
+        };
         _array[i].selector.css("display", _mode)
     }
-}
+};
 
 function get_cookie_cart() {
-    let cookie_cart = {}
+    let cookie_cart = {};
     try {
         cookie_cart = JSON.parse(Cookies.get(cart_cookie))
     } catch (_) {
-    }
+    };
     return cookie_cart
-}
+};
 
 function update_cart_count() {
     $("#count_cart_items_dn").text(countProperties(get_cookie_cart()))
-}
+};
 
 function group_already_in_cart(user_cart) {
-    let cart = Object.keys(user_cart)
+    let cart = Object.keys(user_cart);
     for (let i = 0; i < donate_services_array.length; i++) {
         if (donate_services_array[i].type === "group") {
             if (cart.includes(donate_services_array[i].id.toString())) {
                 return true
             }
         }
-    }
+    };
     return false
-}
+};
 
 function build_players_swiper() {
-    let array_ = $("#players-swiper-array")
+    let array_ = $("#players-swiper-array");
     const create_swiper = function () {
         new Swiper('#players_swipe_container', {
             slidesPerView: 2,
@@ -335,9 +335,9 @@ function build_players_swiper() {
                 }
             }
         })
-    }
+    };
     $.getJSON("assets/data/players.json", function (players) {
-        shuffle(players)
+        shuffle(players);
         for (let i = 0; i < players.length; i++) {
             array_.append(`
                 <div class="swiper-slide">
@@ -351,44 +351,44 @@ function build_players_swiper() {
                     </span>
                 </div>
             `)
-        }
+        };
         create_swiper()
     })
-}
+};
 
 function donate_element_click(product_data) {
-    switch_modal_containers("service")
+    switch_modal_containers("service");
 
-    const exclude_types = ["group"]
-    let modal = document.getElementById("donate_item_modal")
-    let desc = $("#donate_item_select_text")
+    const exclude_types = ["group"];
+    let modal = document.getElementById("donate_item_modal");
+    let desc = $("#donate_item_select_text");
     let text_template = `Вы выбрали товар <span class="text-primary fw-semibold">${product_data.name}</span>, 
         цена ${product_data.count} ${getNoun(product_data.count, "единицы", "единиц", "единиц")} 
         <span class="text-primary fw-semibold">${product_data.price} 
-        ${getNoun(product_data.price, "рубль", "рубля", "рублей")}</span>.`
-    let items_count_donate = $("#items_count_donate")
-    let count_hint = $("#donate_count_text_hint")
-    let add_to_cart = $("#donate_button_add_to_cart")
-    const cookie_cart = get_cookie_cart()
+        ${getNoun(product_data.price, "рубль", "рубля", "рублей")}</span>.`;
+    let items_count_donate = $("#items_count_donate");
+    let count_hint = $("#donate_count_text_hint");
+    let add_to_cart = $("#donate_button_add_to_cart");
+    const cookie_cart = get_cookie_cart();
 
-    let switch_ = false
+    let switch_ = false;
 
     let _update_count = function () {
         add_to_cart.attr(
             "onClick", `donate_cart(${product_data.service_id}, ${items_count_donate.val()})`
         )
-    }
+    };
 
-    items_count_donate.val("1"); _update_count()
+    items_count_donate.val("1"); _update_count();
 
-    const product_in_cart = cookie_cart.hasOwnProperty(product_data.service_id.toString())
+    const product_in_cart = cookie_cart.hasOwnProperty(product_data.service_id.toString());
     if (
         (exclude_types.includes(product_data.type) ||
             product_data.type === "group") && group_already_in_cart(cookie_cart)
     ) {
-        switch_modal_containers("info")
-        switch_ = true
-        let group_error = ""
+        switch_modal_containers("info");
+        switch_ = true;
+        let group_error = "";
         if (product_data.type === "group") {
             group_error = "Вы уже выбрали привилегию. Удалите её из корзины, если хотите выбрать другую."
         } else if (product_in_cart) {
@@ -397,112 +397,112 @@ function donate_element_click(product_data) {
             только один раз.`
         } else {
             group_error = "Мы не знаем почему, но эта ошибка вызвана по неизвестным причинам."
-        }
+        };
         $("#donate_info_block_text").html(group_error)
-    }
+    };
 
-    let count_state = "block"
+    let count_state = "block";
     if (exclude_types.includes(product_data.type)) {
         count_state = "none"
-    }
+    };
 
-    items_count_donate.css("display", count_state)
-    count_hint.css("display", count_state)
+    items_count_donate.css("display", count_state);
+    count_hint.css("display", count_state);
 
     const only_dig = function () {
-        let value = items_count_donate.val()
+        let value = items_count_donate.val();
         items_count_donate.val(
             value.replace(/\D+/g, ''))
-    }
+    };
 
     const _calculate_price = function () {
-        only_dig()
+        only_dig();
         if (!exclude_types.includes(product_data.type)) {
-            let _price = parseInt(items_count_donate.val()) * product_data.count * product_data.price
+            let _price = parseInt(items_count_donate.val()) * product_data.count * product_data.price;
             if (isNaN(_price) || 1 > Math.sign(_price)) {
                 _price = 0
-            }
+            };
             desc.html(`${text_template}<br/>Стоимость выбранного количества - 
             <span class="text-primary fw-semibold">${_price} 
-            ${getNoun(_price, "рубль", "рубля", "рублей")}</span>`)
+            ${getNoun(_price, "рубль", "рубля", "рублей")}</span>`);
             _update_count()
         }
-    }
+    };
 
-    desc.html(text_template)
+    desc.html(text_template);
 
-    _calculate_price()
-    items_count_donate.keyup(_calculate_price)
+    _calculate_price();
+    items_count_donate.keyup(_calculate_price);
 
-    _body.addClass("modal-open")
-    modal.style.display = "block"
+    _body.addClass("modal-open");
+    modal.style.display = "block";
 
     window.onclick = function (event) {
         if (event.target === modal) {
             modal_close_()
         }
     }
-}
+};
 
 function donate_get_service_by_id(id) {
     for (let i = 0; i < donate_services_array.length; i++) {
         if (donate_services_array[i].id === parseInt(id)) {
             return donate_services_array[i]
         }
-    }
+    };
     return null
-}
+};
 
 function donate_cart(product, count, remove = false) {
     if (!Number.isInteger(product) || !Number.isInteger(count)) {
-        console.log("Error data donate_cart")
+        console.log("Error data donate_cart");
         return
-    }
+    };
     if (1 > Math.sign(count)) {
-        notify("Количество не может быть равно нулю или меньше")
+        notify("Количество не может быть равно нулю или меньше");
         return
-    }
-    let cart = Cookies.get(cart_cookie)
+    };
+    let cart = Cookies.get(cart_cookie);
     if (!cart) {
         Cookies.set(cart_cookie, JSON.stringify({}))
-    }
-    const els_ = JSON.parse(Cookies.get(cart_cookie))
-    const product_data = donate_get_service_by_id(product)
-    const local_prm = "<span style=\"color: #a4a6ff !important\">"
+    };
+    const els_ = JSON.parse(Cookies.get(cart_cookie));
+    const product_data = donate_get_service_by_id(product);
+    const local_prm = "<span style=\"color: #a4a6ff !important\">";
     if (remove) {
-        delete els_[product]
+        delete els_[product];
         notify(`Товар ${local_prm} ${product_data.name}</span> убран из корзины`)
     } else {
         if (els_[product]) {
-            els_[product] = els_[product] + count
+            els_[product] = els_[product] + count;
             notify(`В корзину добавлено ${local_prm} ${count} </span> 
                         единиц товара ${local_prm} ${product_data.name} </span>`)
         } else {
-            els_[product] = count
+            els_[product] = count;
             notify(`Товар ${local_prm} ${product_data.name}</span> добавлен в корзину`)
         }
-    }
+    };
 
     Cookies.set(cart_cookie, JSON.stringify(els_));
     modal_close_(); donate_init(); update_cart_count()
-}
+};
 
 function donate_cart_button(els = {}) {
-    const selector_ = $(".donate-cart-button-cn")
+    const selector_ = $(".donate-cart-button-cn");
     if (!jQuery.isEmptyObject(els)) {
-        selector_.css("opacity", 1)
+        selector_.css("opacity", 1);
         selector_.css("margin-top", "15px")
     } else {
-        selector_.css("opacity", 0)
+        selector_.css("opacity", 0);
         selector_.css("margin-top", "-50px")
     }
-}
+};
 
 function donate_flush_cart() {
-    Cookies.remove(cart_cookie)
-    donate_cart_button({})
+    Cookies.remove(cart_cookie);
+    donate_cart_button({});
     notify("Корзина очищена")
-}
+};
 
 function donate_init() {
     let els = {};
@@ -511,7 +511,7 @@ function donate_init() {
     } catch (_) {
     };
     donate_cart_button(els)
-}
+};
 
 function landing_init() {
     const dev_hostnames = [
@@ -521,12 +521,12 @@ function landing_init() {
     if (dev_hostnames.includes(window.location.hostname)) {
         land_desc.text("This is development version!")
     }
-}
+};
 
 function finish_load() {
     $("main").attr("style", "");
     $("footer").attr("style", "")
-}
+};
 
 $(document).ready(function () {
     landing_init(); build_players_swiper(); append_posts(); append_services();
