7ae0423ad17ab830de07b724ab292bd05f506c2e
koval01 <koval01@users.noreply.github.com>
2022-08-20 22:23:15 +0000

    Github Action: Minify CSS and JS

--- assets/js/dark_init.js
+++ assets/js/dark_init.js
@@ -1,18 +1,1 @@
-var global_theme_ = 'light';
-var mode = window.localStorage.getItem('mode'),
-    root = document.getElementsByTagName('html')[0]
-const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)")
-if (darkThemeMq.matches) {
-    mode = 'dark'
-} else {
-    mode = null
-}
-if (mode !== null && mode === 'dark') {
-    root.classList.add('dark-mode')
-    global_theme_ = 'dark'
-    document.querySelector('meta[name="theme-color"]').setAttribute('content', '#0b0f19')
-} else {
-    root.classList.remove('dark-mode')
-    global_theme_ = 'light'
-    document.querySelector('meta[name="theme-color"]').setAttribute('content', '#ffffff')
-}
+var global_theme_="light",mode=window.localStorage.getItem("mode"),root=document.getElementsByTagName("html")[0];const darkThemeMq=window.matchMedia("(prefers-color-scheme: dark)");mode=darkThemeMq.matches?"dark":null,null!==mode&&"dark"===mode?(root.classList.add("dark-mode"),global_theme_="dark",document.querySelector("meta[name=\"theme-color\"]").setAttribute("content","#0b0f19")):(root.classList.remove("dark-mode"),global_theme_="light",document.querySelector("meta[name=\"theme-color\"]").setAttribute("content","#ffffff"));
