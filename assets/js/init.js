1a81f07de9f7d883201c73dadba4ef329333699b
koval01 <yaroslav@koval.page>
2022-09-09 21:09:10 +0200

    update

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,6 +1,89 @@
 const development_hosts = ["zalupa.world", "localhost"];
 const container_dev_splash = document.querySelector(".is-dev-site-splash");
 
+function notify(text) {
+    const error_box = document.querySelector(".error_box_cst");
+    const error_text = document.querySelector(".error_text_cst");
+    const scroll_top = document.querySelector(".btn-scroll-top");
+
+    let notify_hide = function () {
+        error_box.style.marginBottom = "-150px";
+        scroll_top.setAttribute("style", "");
+        notify_hidden = true;
+    };
+
+    let notify_display = function () {
+        notify_hidden = false;
+        error_text.innerHTML = text;
+        scroll_top.style.marginBottom = `calc(${
+            document.getElementById("error_box_cst_id").offsetHeight
+        }px)`;
+        error_box.style.marginBottom = 0;
+    };
+
+    if (notify_hidden) {
+        notify_display();
+    } else {
+        notify_hide();
+        setTimeout(notify_display, 200);
+    }
+
+    clearTimeout(timer_notify);
+    timer_notify = setTimeout(notify_hide, 2500);
+}
+
+function request_call(callback, url, method, json = false, json_body = null) {
+    let request = new XMLHttpRequest();
+    let json_body_local = {};
+    request.open(method, url, true);
+
+    if (method.toUpperCase() === "POST") {
+        request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        json_body_local = JSON.stringify(json_body);
+    }
+
+    request.onload = function () {
+        if (request.status >= 200 && request.status < 400) {
+            if (json) {
+                callback(JSON.parse(request.responseText));
+            } else {
+                callback(request.responseText);
+            }
+        } else {
+            console.log(`Request status code is ${request.status}`);
+        }
+    };
+
+    request.onerror = function (error) {
+        console.log(`Error make request! Details: ${error}`);
+    };
+
+    request.onreadystatechange = () => {
+        if (request.status >= 400) {
+            if (json) {
+                callback({
+                    success: false
+                });
+            } else {
+                callback(null);
+            }
+        }
+    };
+
+    request.send(json_body_local);
+}
+
+function get_last_sha(callback) {
+    request_call(
+        function (r) {
+            return callback(r.sha)
+        },
+        `https://api.github.com/repos/${rep_data.author}/${rep_data.rep_name}/commits/${rep_data.branch}`,
+        "GET",
+        true
+    );
+}
+
 function is_development_splash() {
     document.body.classList.add("modal-open");
     container_dev_splash.style.display = ""
@@ -23,14 +106,16 @@ if (!development_hosts.includes(window.location.hostname)) {
 }
 
 let script_re = document.createElement('script');
-script_re.onload = function () {
-    const scripts = ["theme", "core"];
-    for (let i = 0; i < scripts.length; i++) {
-        let script = document.createElement('script');
-        script.src = `assets/js/${scripts[i]}.min.js`;
-        document.body.appendChild(script);
+get_last_sha(function (sha) {
+    script_re.onload = function () {
+        const scripts = ["theme", "core"];
+        for (let i = 0; i < scripts.length; i++) {
+            let script = document.createElement('script');
+            script.src = `assets/js/${scripts[i]}.min.js?${sha.slice(0, 6)}`;
+            document.body.appendChild(script);
+        }
     }
-}
+})
 
 script_re.src = `https://www.google.com/recaptcha/api.js?render=${re_token}`;
 document.body.appendChild(script_re)
