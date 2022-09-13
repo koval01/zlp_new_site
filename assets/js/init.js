79ec30c2989da9ee8b327dd220993a3b8f081314
koval01 <yaroslav@koval.page>
2022-09-13 14:09:41 +0200

    update

--- assets/js/init.js
+++ assets/js/init.js
@@ -90,7 +90,10 @@ if (!development_hosts.includes(window.location.hostname)) {
     backend_host = `https://${domain_back}`
 } else {
     re_token = "6LfoCqYhAAAAAOLwNkJt45YPE-cCGCZ9ZSqI5Na_";
-    backend_host = "http://127.0.0.1:8000";
+    if (window.location.hostname === "localhost") {
+        backend_host = "http://127.0.0.1:8000"
+    }
+    backend_host = "https://backend.zalupa.world";
     is_development_splash()
 }
 
