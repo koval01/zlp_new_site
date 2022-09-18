7ee82aae1dedb9a58f5dca274bd9a6ed229b2bfa
koval01 <yaroslav@koval.page>
2022-09-18 17:25:45 +0200

    update

--- assets/js/core.js
+++ assets/js/core.js
@@ -289,9 +289,9 @@ function get_game_server_data(callback) {
 function monitoring_game_server_update() {
     get_game_server_data(function (data) {
         if (data.online) {
-            document.getElementById(
-                "server_online_status"
-            ).innerHTML = `Сейчас играет <span class="text-primary fw-semibold">${
+            let selector = document.getElementById("server_online_status");
+            selector.classList.remove("loading-dots");
+            selector.innerHTML = `Сейчас играет <span class="text-primary fw-semibold">${
                 data.online
             }</span>
             <i class="emoji male-emoji" style="margin-left: -.35rem!important;background-image:url('assets/images/emoji/male.png')"><b>♂</b></i>
