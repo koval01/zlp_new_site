aa78030a54ecb596a431c08b5fc13ef5235b5227
Koval Yaroslav <61350874+koval01@users.noreply.github.com>
2022-10-24 21:07:48 +0200

    update

--- assets/js/core.js
+++ assets/js/core.js
@@ -199,21 +199,13 @@ function countProperties(obj) {
 
 function getNoun(number, one = "игрок", two = "игрока", five = "игроков") {
     let n = Math.abs(number);
-    n %= 100;
 
-    if (n >= 5 && n <= 20) {
-        return five;
-    }
+    n %= 100;
+    if (n >= 5 && n <= 20) { return five }
 
     n %= 10;
-
-    if (n === 1) {
-        return one;
-    }
-
-    if (n >= 2 && n <= 4) {
-        return two;
-    }
+    if (n === 1) { return one }
+    if (n >= 2 && n <= 4) { return two }
 
     return five;
 }
