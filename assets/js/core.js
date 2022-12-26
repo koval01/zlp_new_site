6fea4d6cb11988f142357cbf76aa62bd3da3f9f1
Koval Yaroslav <61350874+koval01@users.noreply.github.com>
2022-12-26 15:04:21 +0200

    update

--- assets/js/core.js
+++ assets/js/core.js
@@ -676,7 +676,7 @@ const loadPlayerAvatar = (avatar) => {
     // avatar_style.backgroundImage = `url(${link})`;
 
     testImage(raw_link);
-    avatar_selector.setAttribute("style",`background-image: url("${link}");`);
+    avatar_selector.setAttribute("style",`background-image: url("${link}");border-radius:.3rem;`);
 }
 const reInitTelegramAuth = () => {
     checkTelegramAuthData(function (_) {});
